7f1b721c5a5c9a576a29f56cfda060b49561d013
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-12 17:20:18 +0200

    Install final generic correction architecture

--- comfyui_spectrum_h3/__init__.py
+++ comfyui_spectrum_h3/__init__.py
@@ -1,15 +1,11 @@
 from .config import AGGRESSIVE_PRESET, CONSERVATIVE_PRESET, SpectrumH3Config
-from .feature3_direction import install_feature3_direction_experiment
-from .feature3_mode_boundary import install_feature3_mode_boundary_guard
-from .feature3_previous_error import install_feature3_previous_error_experiment
 from .forecast import HistoryWeightForecaster
+from .generic_correction import install_generic_residual_correction
 from .minimax_h3 import locate_minimax_h3_inner, require_native_minimax_h3
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .runtime import SpectrumH3Runtime
 
-install_feature3_direction_experiment()
-install_feature3_previous_error_experiment()
-install_feature3_mode_boundary_guard()
+install_generic_residual_correction()
 
 __all__ = [
     "AGGRESSIVE_PRESET",
