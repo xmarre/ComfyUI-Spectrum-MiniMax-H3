fe9ff4ea3f81ee521d75a3f6d996422ed042809e
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-12 14:41:26 +0200

    Install normalized Feature 3 direction screen

--- comfyui_spectrum_h3/__init__.py
+++ comfyui_spectrum_h3/__init__.py
@@ -1,11 +1,13 @@
 from .config import AGGRESSIVE_PRESET, CONSERVATIVE_PRESET, SpectrumH3Config
 from .feature3_direction import install_feature3_direction_experiment
+from .feature3_direction_normalization import install_feature3_direction_normalization
 from .forecast import HistoryWeightForecaster
 from .minimax_h3 import locate_minimax_h3_inner, require_native_minimax_h3
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .runtime import SpectrumH3Runtime
 
 install_feature3_direction_experiment()
+install_feature3_direction_normalization()
 
 __all__ = [
     "AGGRESSIVE_PRESET",
