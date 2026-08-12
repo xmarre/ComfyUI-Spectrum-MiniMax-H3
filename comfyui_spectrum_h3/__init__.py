c186602fbdf82b73cafc53f6e8bebe304eb77a41
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-12 13:37:44 +0200

    Sort Feature-3 package imports

--- comfyui_spectrum_h3/__init__.py
+++ comfyui_spectrum_h3/__init__.py
@@ -1,9 +1,9 @@
 from .config import AGGRESSIVE_PRESET, CONSERVATIVE_PRESET, SpectrumH3Config
+from .feature3_direction import install_feature3_direction_experiment
 from .forecast import HistoryWeightForecaster
 from .minimax_h3 import locate_minimax_h3_inner, require_native_minimax_h3
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .runtime import SpectrumH3Runtime
-from .feature3_direction import install_feature3_direction_experiment
 
 install_feature3_direction_experiment()
 
