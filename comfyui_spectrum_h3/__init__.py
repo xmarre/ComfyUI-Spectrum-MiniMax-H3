b52a171fda394e53dafd74a50490f365e9d573e9
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-12 16:01:02 +0200

    Install strict Feature-3 mode guard

--- comfyui_spectrum_h3/__init__.py
+++ comfyui_spectrum_h3/__init__.py
@@ -1,5 +1,6 @@
 from .config import AGGRESSIVE_PRESET, CONSERVATIVE_PRESET, SpectrumH3Config
 from .feature3_direction import install_feature3_direction_experiment
+from .feature3_mode_boundary import install_feature3_mode_boundary_guard
 from .feature3_previous_error import install_feature3_previous_error_experiment
 from .forecast import HistoryWeightForecaster
 from .minimax_h3 import locate_minimax_h3_inner, require_native_minimax_h3
@@ -8,6 +9,7 @@ from .runtime import SpectrumH3Runtime
 
 install_feature3_direction_experiment()
 install_feature3_previous_error_experiment()
+install_feature3_mode_boundary_guard()
 
 __all__ = [
     "AGGRESSIVE_PRESET",
